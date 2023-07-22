4cadc21968df32870014042491da34609a49da57
João David Nóbrega <jnobrega@neoception.com>
2023-07-22 18:47:38 +0100

    chore: small refactor

--- scripts/k3sup.sh
+++ scripts/k3sup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-K3S_VERSION="v1.24.7+k3s1"
+K3S_VERSION="v1.25.11+k3s1"
 K3S_SERVER_IP="192.168.1.80"
 K3S_USER="server"
 LOCAL_KUBECONFIG="~/.kube/home-cluster"
-k3sup install --ip $K3S_SERVER_IP --user $K3S_USER --k3s-version $K3S_VERSION --k3s-extra-args "--no-deploy servicelb --no-deploy traefik" --local-path="$LOCAL_KUBECONFIG" --ssh-key "/Users/jnobrega/.ssh/id_ed25519"
+k3sup install --ip $K3S_SERVER_IP --user $K3S_USER --k3s-version $K3S_VERSION --k3s-extra-args "--disable servicelb --disable traefik" --local-path="$LOCAL_KUBECONFIG" --ssh-key "/Users/jnobrega/.ssh/id_ed25519"
