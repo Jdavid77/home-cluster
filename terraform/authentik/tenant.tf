7c49f90e875966cf030215e93ba242f569572023
jnobrega <jnobrega@neoception.com>
2023-10-01 22:38:58 +0100

    chore: customize authentik tenant

--- terraform/authentik/tenant.tf
+++ terraform/authentik/tenant.tf
@@ -2,7 +2,7 @@ resource "authentik_tenant" "home-cluster" {
   domain              = "authentik.${var.external_host}"
   default             = true
   branding_title      = "Home-Cluster"
-  branding_favicon    = "https://icons8.com/icon/91350/lock"
+  branding_favicon    = "https://i.postimg.cc/7hs05Q2R/icons8-lock-66.png"
   branding_logo       = "https://i.postimg.cc/JzSPht3L/logo-3.png"
   flow_authentication = data.authentik_flow.default-authentication-flow.id
   flow_invalidation   = data.authentik_flow.default-invalidation-flow.id
