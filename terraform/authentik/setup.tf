91bb7e69848e3d50419e6fb33c197b3a31e7a00e
jnobrega <jnobrega@neoception.com>
2023-10-02 12:11:43 +0100

    feat: add proxy to calibre web

--- terraform/authentik/setup.tf
+++ terraform/authentik/setup.tf
@@ -26,6 +26,12 @@ module "home-cluster" {
       authentik_proxy_application_icon_url = "https://play-lh.googleusercontent.com/209fPBA5Q2ceVu4hIDE2x4sE3-FQPQd4lJMPWv0ckZ4yH-K6fRfRt1ZilDxG3-AmzPDX=w240-h480-rw"
       authentik_proxy_external_host        = "https://actual.${var.external_host}"
     },
+    {
+      authentik_proxy_application_name     = "calibre-web"
+      authentik_proxy_application_group    = "home-cluster"
+      authentik_proxy_application_icon_url = "https://developer.asustor.com/uploadIcons/0020_999_1595583564_calibre-web_256.png"
+      authentik_proxy_external_host        = "https://calibre-web.${var.external_host}"
+    },
   ]
 
   providers = {
