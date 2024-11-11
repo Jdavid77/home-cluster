df1e9685a449cd856e807e218e8600f4bfe9d770
jnobrega <jnobrega@neoception.com>
2024-11-11 11:49:54 +0000

    chore: update bootstrap script

--- k8s/bootstrap/flux/script.sh
+++ k8s/bootstrap/flux/script.sh
@@ -4,3 +4,9 @@ kubectl create namespace flux-system
 sops -d age-key.sops.yaml | kubectl apply -f -
 sops -d github-deploy-key.sops.yaml | kubectl apply -f -
 kubectl apply -k .
+
+cd ../../flux/vars
+sops -d cluster-secrets.sops.yaml | kubectl apply -f -
+
+cd ../config
+kubectl apply -k .
