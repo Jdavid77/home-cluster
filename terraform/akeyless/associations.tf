6a57889da67ebbcc126e485f3223fa7e74e7bfb1
jnobrega <jnobrega@neoception.com>
2024-12-05 17:22:51 +0000

    chore: change remote location of push secret

--- terraform/akeyless/associations.tf
+++ terraform/akeyless/associations.tf
@@ -1,5 +1,9 @@
-resource "akeyless_associate_role_auth_method" "this" {
+resource "akeyless_associate_role_auth_method" "ro" {
   am_name   = akeyless_auth_method_api_key.this.name
-  role_name = akeyless_role.this.name
+  role_name = akeyless_role.ro.name
+}
 
+resource "akeyless_associate_role_auth_method" "cm-w" {
+  am_name   = akeyless_auth_method_api_key.this.name
+  role_name = akeyless_role.cm-w.name
 }
--- terraform/akeyless/roles.tf
+++ terraform/akeyless/roles.tf
@@ -1,10 +1,22 @@
-resource "akeyless_role" "this" {
+resource "akeyless_role" "ro" {
 
   name = "ReadOnly"
 
   rules {
-    capability = ["read", "list", "create"]
+    capability = ["read", "list"]
     path       = "/*"
+    rule_type  = "item-rule"
+  }
+
+}
+
+resource "akeyless_role" "cm-w" {
+
+  name = "CM-Read-Write"
+
+  rules {
+    capability = ["create","update","read","delete","list"]
+    path       = "/cert-manager/*"
     rule_type  = "item-rule"
   }
 
