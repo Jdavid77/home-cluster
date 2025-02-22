c56c84bc9e382012a2a192e3cd2c153d55918bee
jnobrega <jnobrega@neoception.com>
2025-02-22 19:50:27 +0000

    chore: make use of github provider for git repos

--- k8s/bootstrap/flux/script.sh
+++ k8s/bootstrap/flux/script.sh
@@ -3,6 +3,7 @@
 kubectl create namespace flux-system
 sops -d age-key.sops.yaml | kubectl apply -f -
 sops -d github-deploy-key.sops.yaml | kubectl apply -f -
+sops -d github-app.sops.yaml | kubectl apply -f -
 kubectl apply -k .
 
 cd ../../flux/vars
