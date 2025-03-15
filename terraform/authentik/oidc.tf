509f1fbc1979628d1b6c9fbaab7d782df6e7dafc
jnobrega <jnobrega@neoception.com>
2025-03-15 15:14:41 +0000

    chore: remove authentik collection

--- terraform/authentik/oidc.tf
+++ terraform/authentik/oidc.tf
@@ -19,6 +19,11 @@ locals {
       authentik_oidc_application_name        = "Backstage-Dev"
       authentik_oidc_application_icon_url    = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBzHHyxg-LtRQ5iGOxRQeP5VzxFoNjzDY3Lg&s"
       authentik_oidc_application_description = "Backstage-Dev OAuth application"
+    },
+    {
+      authentik_oidc_application_name        = "Adventurelog"
+      authentik_oidc_application_icon_url    = "https://github.com/seanmorley15/AdventureLog/raw/main/brand/adventurelog.png"
+      authentik_oidc_application_description = "AdventureLog OAuth application"
     }
   ]
 }
