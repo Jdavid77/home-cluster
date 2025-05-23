12d6025b2df89b6bf0379be99ab3c36b113c36c8
Jdavid77 <johnynobrega17@gmail.com>
2025-05-23 10:23:43 +0100

    chore: update secret store apiversion

--- k8s/bootstrap/flux/script.sh
+++ k8s/bootstrap/flux/script.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-kubectl create namespace flux-system
 sops -d age-key.sops.yaml | kubectl apply -f -
 sops -d github-app.sops.yaml | kubectl apply -f -
-kubectl apply -k .
 
 cd ../../flux/vars
 sops -d cluster-secrets.sops.yaml | kubectl apply -f -
-
-cd ../config
-kubectl apply -k .
