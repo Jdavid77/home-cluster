a09b3ba470246b7eb195c683ad3f9280fbc9c923
joao.nobrega <joao.nobrega@definedcrowd.com>
2026-05-02 10:12:55 +0100

    fix: resource names

--- terraform/garage/main.tf
+++ terraform/garage/main.tf
@@ -1,15 +1,15 @@
 resource "garage_bucket" "this" {
-  for_each = var.buckets
-  name     = each.value
+  for_each     = var.buckets
+  global_alias = each.value
 }
 
-resource "garage_access_key" "this" {
+resource "garage_key" "this" {
   name          = "main"
   never_expires = true
 }
 
 # just one key with access to all buckets, couldn't care less lol
-resource "garage_permission" "this" {
+resource "garage_bucket_permission" "this" {
   for_each      = var.buckets
   access_key_id = garage_access_key.this.id
   bucket_id     = garage_bucket.this[each.key].id
