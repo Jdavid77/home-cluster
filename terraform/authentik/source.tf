6893f6811d081c2214211ecb7ec5491c3924e62e
joao.nobrega <joao.nobrega@definedcrowd.com>
2026-05-08 14:33:30 +0100

    chore: remove it-tools

--- terraform/authentik/source.tf
+++ terraform/authentik/source.tf
@@ -20,20 +20,20 @@ resource "authentik_source_ldap" "this" {
   name = "lldap"
   slug = "lldap"
 
-  server_uri              = "ldap://lldap.security.svc.cluster.local"
-  bind_cn                 = local.bind_cn
-  bind_password           = local.bind_password
-  base_dn                 = "dc=jnobrega,dc=com"
-  user_path_template      = "LDAP/users"
-  property_mappings       = data.authentik_property_mapping_source_ldap.this.ids
-  property_mappings_group = data.authentik_property_mapping_source_ldap.group.ids
-  additional_user_dn      = "ou=people"
-  additional_group_dn     = "ou=groups"
-  user_object_filter      = "(objectClass=person)"
-  group_object_filter     = "(objectClass=groupOfUniqueNames)"
-  group_membership_field  = "member"
-  object_uniqueness_field = "uid"
-  start_tls               = false
+  server_uri               = "ldap://lldap.security.svc.cluster.local"
+  bind_cn                  = local.bind_cn
+  bind_password            = local.bind_password
+  base_dn                  = "dc=jnobrega,dc=com"
+  user_path_template       = "LDAP/users"
+  property_mappings        = data.authentik_property_mapping_source_ldap.this.ids
+  property_mappings_group  = data.authentik_property_mapping_source_ldap.group.ids
+  additional_user_dn       = "ou=people"
+  additional_group_dn      = "ou=groups"
+  user_object_filter       = "(objectClass=person)"
+  group_object_filter      = "(objectClass=groupOfUniqueNames)"
+  group_membership_field   = "member"
+  object_uniqueness_field  = "uid"
+  start_tls                = false
   delete_not_found_objects = true
 
 }
